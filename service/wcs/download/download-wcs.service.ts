117f9a54a64615f68753990446af02f6db40e0b3
Victor Tey <victor.tey@csiro.au>
2018-01-31 16:18:50 +0800

    wcs download

--- service/wcs/download/download-wcs.service.ts
+++ service/wcs/download/download-wcs.service.ts
@@ -1,5 +1,6 @@
 import { Bbox } from '../../../model/data/bbox.model';
 import { LayerModel } from '../../../model/data/layer.model';
+import { Constants } from '../../../utility/constants.service';
 import { LayerHandlerService } from '../../cswrecords/layer-handler.service';
 import {HttpClient, HttpParams, HttpHeaders} from '@angular/common/http';
 import {Injectable, Inject} from '@angular/core';
@@ -25,44 +26,42 @@ export class DownloadWcsService {
    * @param the layer to download
    * @param bbox the bounding box of the area to download
    */
-  public download(layer: LayerModel, bbox: Bbox) {
+  public download(layer: LayerModel, bbox: Bbox, inputCrs: string, downloadFormat: string, outputCrs: string): Observable<any> {
+    try {
+      const ftpResources = this.layerHandlerService.getOnlineResources(layer, Constants.resourceType.FTP);
+      const ftpURL = (ftpResources.length > 0) ? ftpResources[0]['url'] : '';
+      const wcsResources = this.layerHandlerService.getWCSResource(layer);
 
-    const wfsResources = this.layerHandlerService.getWFSResource(layer);
+      let httpParams = new HttpParams();
+      httpParams = httpParams.set('layerName', wcsResources[0].name);
+      httpParams = httpParams.set('serviceUrl', wcsResources[0].url);
+      httpParams = httpParams.set('usingBboxConstraint', 'on');
+      httpParams = httpParams.set('northBoundLatitude', bbox.northBoundLatitude.toString());
+      httpParams = httpParams.set('southBoundLatitude', bbox.southBoundLatitude.toString());
+      httpParams = httpParams.set('eastBoundLongitude', bbox.eastBoundLongitude.toString());
+      httpParams = httpParams.set('westBoundLongitude', bbox.westBoundLongitude.toString());
 
-    let downloadUrl = 'getAllFeaturesInCSV.do';
-    if (layer.proxyDownloadUrl && layer.proxyDownloadUrl.length > 0) {
-      downloadUrl =  layer.proxyDownloadUrl;
-    } else if (layer.proxyUrl && layer.proxyUrl.length > 0) {
-      downloadUrl =  layer.proxyUrl;
+      httpParams = httpParams.set('outputDimensionsType', 'widthHeight');
+      httpParams = httpParams.set('outputWidth', '256');
+      httpParams = httpParams.set('outputHeight', '256');
+
+      httpParams = httpParams.set('inputCrs', inputCrs);
+      httpParams = httpParams.set('downloadFormat', downloadFormat);
+      httpParams = httpParams.set('outputCrs', outputCrs);
+      httpParams = httpParams.set('ftpURL', ftpURL);
+
+      return this.http.get(this.env.portalBaseUrl + 'downloadWCSAsZip.do', {
+        params: httpParams,
+        responseType: 'blob'
+      }).timeoutWith(360000, Observable.throw(new Error('Request have timeout out after 5 minutes')))
+        .map((response) => { // download file
+          return response;
+        }).catch((error: Response) => {
+          return Observable.throw(error);
+        });
+    } catch (e) {
+      return Observable.throw(e);
     }
-
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set('outputFormat', 'csv');
-
-    for (let i = 0; i < wfsResources.length; i++) {
-      const filterParameters = {
-        serviceUrl: wfsResources[i].url,
-        typeName: wfsResources[i].name,
-        maxFeatures: 0,
-        outputFormat: 'csv',
-        bbox: bbox ? JSON.stringify(bbox) : ''
-      };
-
-      const serviceUrl = this.env.portalBaseUrl + downloadUrl + '?';
-
-
-      httpParams = httpParams.append('serviceUrls', serviceUrl + $.param(filterParameters));
-    }
-
-    return this.http.get(this.env.portalBaseUrl + 'downloadGMLAsZip.do', {
-      params: httpParams,
-      responseType: 'blob'
-    }).timeoutWith(360000, Observable.throw(new Error('Request have timeout out after 5 minutes')))
-      .map((response) => { // download file
-      return response;
-    }).catch((error: Response) => {
-        return Observable.throw(error);
-    });
 
   }
 
--- service/wfs/download/download-wfs.service.ts
+++ service/wfs/download/download-wfs.service.ts
@@ -25,44 +25,48 @@ export class DownloadWfsService {
    * @param the layer to download
    * @param bbox the bounding box of the area to download
    */
-  public download(layer: LayerModel, bbox: Bbox) {
+  public download(layer: LayerModel, bbox: Bbox): Observable<any> {
 
-    const wfsResources = this.layerHandlerService.getWFSResource(layer);
+    try {
+      const wfsResources = this.layerHandlerService.getWFSResource(layer);
 
-    let downloadUrl = 'getAllFeaturesInCSV.do';
-    if (layer.proxyDownloadUrl && layer.proxyDownloadUrl.length > 0) {
-      downloadUrl =  layer.proxyDownloadUrl;
-    } else if (layer.proxyUrl && layer.proxyUrl.length > 0) {
-      downloadUrl =  layer.proxyUrl;
+      let downloadUrl = 'getAllFeaturesInCSV.do';
+      if (layer.proxyDownloadUrl && layer.proxyDownloadUrl.length > 0) {
+        downloadUrl = layer.proxyDownloadUrl;
+      } else if (layer.proxyUrl && layer.proxyUrl.length > 0) {
+        downloadUrl = layer.proxyUrl;
+      }
+
+      let httpParams = new HttpParams();
+      httpParams = httpParams.set('outputFormat', 'csv');
+
+      for (let i = 0; i < wfsResources.length; i++) {
+        const filterParameters = {
+          serviceUrl: wfsResources[i].url,
+          typeName: wfsResources[i].name,
+          maxFeatures: 0,
+          outputFormat: 'csv',
+          bbox: bbox ? JSON.stringify(bbox) : ''
+        };
+
+        const serviceUrl = this.env.portalBaseUrl + downloadUrl + '?';
+
+
+        httpParams = httpParams.append('serviceUrls', serviceUrl + $.param(filterParameters));
+      }
+
+      return this.http.get(this.env.portalBaseUrl + 'downloadGMLAsZip.do', {
+        params: httpParams,
+        responseType: 'blob'
+      }).timeoutWith(360000, Observable.throw(new Error('Request have timeout out after 5 minutes')))
+        .map((response) => { // download file
+          return response;
+        }).catch((error: Response) => {
+          return Observable.throw(error);
+        });
+    } catch (e) {
+      return Observable.throw(e);
     }
-
-    let httpParams = new HttpParams();
-    httpParams = httpParams.set('outputFormat', 'csv');
-
-    for (let i = 0; i < wfsResources.length; i++) {
-      const filterParameters = {
-        serviceUrl: wfsResources[i].url,
-        typeName: wfsResources[i].name,
-        maxFeatures: 0,
-        outputFormat: 'csv',
-        bbox: bbox ? JSON.stringify(bbox) : ''
-      };
-
-      const serviceUrl = this.env.portalBaseUrl + downloadUrl + '?';
-
-
-      httpParams = httpParams.append('serviceUrls', serviceUrl + $.param(filterParameters));
-    }
-
-    return this.http.get(this.env.portalBaseUrl + 'downloadGMLAsZip.do', {
-      params: httpParams,
-      responseType: 'blob'
-    }).timeoutWith(360000, Observable.throw(new Error('Request have timeout out after 5 minutes')))
-      .map((response) => { // download file
-      return response;
-    }).catch((error: Response) => {
-        return Observable.throw(error);
-    });
 
   }
 }
